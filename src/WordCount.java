02a01f2f52b7b4f408ce7d3ac74a8fa7f027abb8
SthlmJ <joehuld1@gmail.com>
2013-06-10 21:38:24 +0200

    cmiitt

--- src/WordCount.java
+++ src/WordCount.java
@@ -2,27 +2,38 @@
  * WordCount
  * Labb 4 Uppgift 7
  *  
+ *  /Users/joedan/Documents/EclipseWorkspace/Search/src/words.txt
  */
 
  import java.io.*;  
  import java.util.*;  
+
  class WordCount {  
-   public static void main(String[] unused) {  
+	 
+   public static void main(String[] unused) {
+	   
+	   /**
+	    *Make scanner object 
+	    */
      Scanner in = new Scanner(System.in); // scanner object.   
-      int awords=0,uwords=0;   
+     int awords=0,uwords=0;   
+     
+     /**
+      * try to read in a file name with exception handling.
+      */
       try{  
            System.out.println("Enter your file name");  
-           File inputfile = new File(in.nextLine());  
-           Scanner words = new Scanner(inputfile);   
-           words.useDelimiter("[^A-Za-z]+");  
-           HashMap<String,Integer> words_fre = new HashMap<String,Integer>();  
-           while(words.hasNext())  
+           File inputfile = new File(in.nextLine());  //file
+           Scanner words = new Scanner(inputfile);   //file to words
+           words.useDelimiter("[^A-Za-z]+");  //ignore cases
+           HashMap<String,Integer> words_fre = new HashMap<String,Integer>();	//make hash object  
+           while(words.hasNext())  //while we have a word
                 {  
-                     String s = words.next().toString();   
-                     awords++; // words count   
-                     if(words_fre.containsKey(s))  
+                     String s = words.next().toString(); 	//words to string  
+                     awords++; // word counts   
+                     if(words_fre.containsKey(s))	//if duplicates a increment 
                           {  
-                               int a = words_fre.get(s);  
+                               int a = words_fre.get(s);
                                words_fre.put(s,a+1);             
                           }  
                      else {  
@@ -30,19 +41,14 @@
                           uwords++; // unique words count   
                         }  
                 }  
-       Object[] key =   words_fre.keySet().toArray();   
+           /**
+            * put word counts to an array and prints unique and duplicated words
+            */
+       Object[] key =   words_fre.keySet().toArray();
        Arrays.sort(key);  
-       for (int i = 0; i < key.length; i++) {  
-      System.out.println(key[i]+"= "+words_fre.get(key[i]));  
+       for (int i = 0; i < key.length; i++) {  //for print
+      System.out.println(key[i]+"= "+words_fre.get(key[i]));  //print words
       }  
-           /*//words_fre = getSortedMap(words_fre);  
-           Set<String> keys = words_fre.keySet();  
-           Iterator list = keys.iterator();   
-           while(list.hasNext())  
-           {  
-                String ss = list.next().toString();   
-                System.out.println(ss+" = "+words_fre.get(ss));  
-           }*/  
            System.out.println("Total Words = "+awords);  
            System.out.println("Unique Words = "+words_fre.size());  
         }catch(IOException e)  
